1904c965faa70810092af8bc2ccf06711f1fd8d1
ha1t <halt.feits@gmail.com>
2011-04-28 20:38:05 +0900

    version numberあげた

--- rakuten_search.php
+++ rakuten_search.php
@@ -4,14 +4,14 @@ Plugin Name: WP Rakuten Tag
 Plugin URI: http://blog.newf.jp/myplugin/wp-rakuten-link/
 Description: [rakuten][/rakuten]で囲まれたwordから楽天市場の個別商品を検索しエントリー上に表示します。利用にあたっては、楽天ウェブサービスのデベロッパーIDが別途必要です。WP Rakuten Linkを参考にさせていただきました。
 Author: halt
-Version: 0.0.2
+Version: 0.0.3
 Author URI: http://project-p.jp/halt/
 
 [更新履歴]
+2012/04/28 0.0.3 : かろうじて動くように
 2012/04/27 0.0.2 : githubに公開
 
 @todo モバイルの時はモバイルのページに飛ばすようにする
-@todo 24h キャッシュをつける
 @todo Services_Rakutenへの依存をやめる
  */
 
