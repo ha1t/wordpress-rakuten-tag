e31ab97a5052497125e4324cf8960e4a551829b7
ha1t <halt.feits@gmail.com>
2014-06-12 15:37:19 +0900

    add private static method test

--- tests/RakutenTagTest.php
+++ tests/RakutenTagTest.php
@@ -10,5 +10,11 @@ class RakutenTagTest extends PHPUnit_Framework_TestCase
         $RakutenTag = new RakutenTag();
         $this->assertEquals('rakutentag', strtolower(get_class($RakutenTag)));
     }
+
+    public function testCreateCachePath()
+    {
+        $result = Test_Util::invokeStaticMethod('RakutenTag', 'createCachePath', array('a/b'));
+        $this->assertEquals('a_slash_b.txt', basename($result));
+    }
 }
 
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,22 @@
 <?php
+
+class Test_Util
+{
+  // $class_obj : テストするクラスのオブジェクト(new CLASS_NAME())
+  // $method_name : 呼び出すメソッド名のstring
+  // $args : 呼び出すメソッドの引数
+  //    array('args1' => 'hogehoge', 'args2' => 'fugafuga', ...)
+  public static function invokeStaticMethod($class_obj, $method_name, $args)
+  {
+    $test_class = new ReflectionClass($class_obj);
+    $method = $test_class->getMethod($method_name);
+    // メソッドを外からアクセス可能にする
+    $method->setAccessible(true);
+    // invokeArgsメソッドの第一引数をnullにするとstaticメソッドを呼び出せる
+    return $method->invokeArgs(null, $args);
+  }
+}
+
 require_once dirname(dirname(__FILE__)) . '/includes/RakutenTag.php';
 require_once dirname(dirname(__FILE__)) . '/includes/RakutenTagAdmin.php';
 
